a37ce6487ff04d533c726fc5ed0fca84567832ca
FrancescoNegri <francesco.negri@outlook.com>
2018-09-19 20:24:58 +0800

    Finished pipeType

--- www/js/game.js
+++ www/js/game.js
@@ -6,6 +6,8 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.spritesheet('bird', 'img/bird.png', { frameWidth: 50, frameHeight: 50 });
         this.load.image('pipe', 'img/pipe.png');
+        this.load.image('pipe-up', 'img/pipe-up.png');
+        this.load.image('pipe-down', 'img/pipe-down.png');
     }
 
     create() {
@@ -62,47 +64,51 @@ class GameScene extends Phaser.Scene {
     }
 
     addRowofPipes() {
-        var hole = Math.floor(Math.random() * 5) + 1;
-        for (var i = 0; i < 8; i++) {
-            var pipeType='pipe';
+        const COLUMN_SIZE = 9;
+        const PIPE_SIZE = 60;
+        //Calcolre 3 dinamicamente (se l'altezza della webview è esattamente div per 60, allora non posso usare solo l'ultimo buco (-3), se è arrotondata per eccesso allora è (-4))
+        var hole = Math.floor(Math.random() * (COLUMN_SIZE - 3)) + 1;
+        for (var i = 0; i < COLUMN_SIZE; i++) {
+            var pipeType = 'pipe';
             if (i != hole && i != hole + 1) {
-                if (i == hole - 1) pipeType='pipe-up';
-                else if (i == hole + 2) pipeType='pipe-down';
-            this.addOnePipe(400, i * 60 + 10, pipeType);
-        }
-            this.score++;
-            this.labelScore.text = this.score - 1;
-        }
-
-        jump() {
-            if (this.bird.alive) {
-                this.bird.body.velocity.y = -350;
-                var animation = this.tweens.add({
-                    targets: this.bird, angle: -20, duration: 100, callback: () => {
-                        console.log(this.bird.angle);
-                    }
-                });
+                if (i == hole - 1) pipeType = 'pipe-up';
+                else if (i == hole + 2) pipeType = 'pipe-down';
+                this.addOnePipe(400, i * PIPE_SIZE, pipeType);
             }
         }
+        this.score++;
+        this.labelScore.text = this.score - 1;
+    }
 
-        restart() {
-            GAME.scene.stop('GameScene');
-            GAME.scene.run('GameScene');
-        }
-
-        hitPipe() {
-            if (this.bird.alive) {
-                this.bird.play('die');
-                this.bird.alive = false;
-
-                this.timer.destroy();
-
-                this.pipes.getChildren().forEach(pipe => {
-                    pipe.body.setVelocityX(0);
-                });
-            }
+    jump() {
+        if (this.bird.alive) {
+            this.bird.body.velocity.y = -350;
+            var animation = this.tweens.add({
+                targets: this.bird, angle: -20, duration: 100, callback: () => {
+                    console.log(this.bird.angle);
+                }
+            });
         }
     }
+
+    restart() {
+        GAME.scene.stop('GameScene');
+        GAME.scene.run('GameScene');
+    }
+
+    hitPipe() {
+        if (this.bird.alive) {
+            this.bird.play('die');
+            this.bird.alive = false;
+
+            this.timer.destroy();
+
+            this.pipes.getChildren().forEach(pipe => {
+                pipe.body.setVelocityX(0);
+            });
+        }
+    }
+}
 
 function resize() {
     var canvas = GAME.canvas, width = window.innerWidth, height = window.innerHeight;
